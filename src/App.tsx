489e828aed6041d7dbfb6e4234c8bdafaa75eadf
Laping Miklos-Jozsef <lapingmikus@gmail.com>
2024-06-10 17:39:36 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// import logo from "./assets/logo.svg"
+import "./App.css";
+import { TitleBar } from "./components/TitleBar";
+
+function App() {
+  return (
+    <>
+      <TitleBar />
+      <div class="container">
+        <h1>Welcome to Kvasir!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/TitleBar.tsx
@@ -0,0 +1,42 @@
+import { appWindow } from "@tauri-apps/api/window";
+import windowMax from "../assets/window-max.svg";
+import windowMin from "../assets/window-min.svg";
+import windowClose from "../assets/window-close.svg";
+import logo from "../assets/logo.svg";
+import "../css/TitleBar.css";
+
+export function TitleBar() {
+  return (
+    <div data-tauri-drag-region class="titlebar">
+      <div class="titlebar-icon">
+        <img src={logo} alt="minimize" />
+      </div>
+      <div data-tauri-drag-region class="bar" id="bar"></div>
+      <div class="titlebar-buttons">
+        <div
+          class="titlebar-button"
+          id="titlebar-minimize"
+          onClick={appWindow.minimize}
+        >
+          <img src={windowMin} alt="minimize" />
+        </div>
+
+        <div
+          class="titlebar-button"
+          id="titlebar-maximize"
+          onClick={appWindow.toggleMaximize}
+        >
+          <img src={windowMax} alt="maximize" />
+        </div>
+
+        <div
+          class="titlebar-button"
+          id="titlebar-close"
+          onClick={appWindow.close}
+        >
+          <img src={windowClose} alt="close" />
+        </div>
+      </div>
+    </div>
+  );
+}
