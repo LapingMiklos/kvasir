690d8697c7f85433418e37234f4d48ae78c6a11b
Laping Miklos-Jozsef <lapingmikus@gmail.com>
2024-06-12 12:55:38 +0300

    created database

--- src-tauri/src/error.rs
+++ src-tauri/src/error.rs
@@ -1,5 +1,13 @@
+use serde_json::error;
+
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
     #[error("Generic {0}")]
     Generic(String),
+
+    #[error(transparent)]
+    IO(#[from] std::io::Error),
+
+    #[error(transparent)]
+    DB(#[from] sqlx::Error),
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,8 +2,11 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 #![allow(unused)] // COMMENT OUT LATER
 
+use std::fs;
+
 use crate::prelude::*;
 use model::out::spell::SpellView;
+use sqlx::{migrate::MigrateDatabase, Sqlite, SqlitePool};
 use ts_rs::TS;
 
 mod error;
@@ -11,13 +14,27 @@ mod model;
 mod prelude;
 
 const EXPORT_DIR: &'static str = "../src/types/ts-rs";
-const DB_URL: &'static str = "sqlite://sqlite.db";
+const DB_URL: &'static str = "sqlite://runtime_res/sqlite.db";
 
 #[tokio::main]
 async fn main() -> Result<()> {
     // let _ = Spell::export_all_to(EXPORT_DIR);
     // let _ = SpellLevel::export_all_to(EXPORT_DIR);
     let _ = SpellView::export_all_to(EXPORT_DIR);
+
+    fs::create_dir("runtime_res");
+
+    if !Sqlite::database_exists(DB_URL).await? {
+        println!("Creating database {}", DB_URL);
+        match Sqlite::create_database(DB_URL).await {
+            Ok(_) => println!("Create db success"),
+            Err(err) => println!("Error: {err}",),
+        }
+    } else {
+        println!("Database already exists");
+    }
+
+    let db = SqlitePool::connect(DB_URL).await?;
 
     tauri::Builder::default()
         .run(tauri::generate_context!())
