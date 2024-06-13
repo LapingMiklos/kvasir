11171c854703f4f8c72107fb05b7473645e653e4
Laping Miklos-Jozsef <lapingmikus@gmail.com>
2024-06-13 20:53:02 +0300

    added post_spell command to the frontend

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,43 @@ import { TitleBar } from "./components/TitleBar";
 import SpellCard from "./components/spell/SpellCard";
 import { SpellView } from "./types/ts-rs/SpellView";
 import invokeCommand from "./commands/invokeCommand";
-import { GetSpells } from "./commands/spellCommands";
+import { GetSpells, PostSpell } from "./commands/spellCommands";
 
 function App() {
   const [spells, setSpells] = createSignal<SpellView[]>([]);
 
   onMount(async () => {
+    const postRes = await invokeCommand<PostSpell>("post_spell", {
+      spell: {
+        name: "Ice Knife",
+        iconUrl: "https://www.dndbeyond.com/attachments/2/708/conjuration.png",
+        area: { size: 5, shape: "sphere" },
+        atHigherLevel:
+          "When you cast this spell using a spell slot of 2nd level or higher, the cold damage increases by 1d6 for each slot level above 1st.",
+        attackSave: { save: "DEX" },
+        castTime: "action",
+        description:
+          "You create a shard of ice and fling it at one creature within range. Make a ranged spell attack against the target. On a hit, the target takes 1d10 piercing damage. Hit or miss, the shard then explodes. The target and each creature within 5 feet of it must succeed on a Dexterity saving throw or take 2d6 cold damage.",
+        duration: "instantaneous",
+        effect: "piercing",
+        hasMultipleEffects: true,
+        isSomatic: true,
+        isVerbal: false,
+        materials: "a drop of water or a piece of ice",
+        level: { leveled: 1 },
+        range: { distance: 60 },
+        school: "conjuration",
+        spellDice: [
+          { base: 1, damageType: "piercing", dice: "d10", scaling: 0 },
+          { base: 2, damageType: "cold", dice: "d6", scaling: 1 },
+        ],
+      },
+    });
+
+    if (postRes.error != null) {
+      console.log(postRes.error.message);
+    }
+
     const res = await invokeCommand<GetSpells>("get_spells", {});
     if (res.error == null) {
       setSpells(res.result.data);
--- src/commands/spellCommands.ts
+++ src/commands/spellCommands.ts
@@ -1,4 +1,7 @@
+import { CreateSpell } from "../types/ts-rs/CreateSpell";
 import { SpellView } from "../types/ts-rs/SpellView";
 import { BaseCommand } from "./invokeCommand";
 
 export type GetSpells = BaseCommand<"get_spells", {}, SpellView[]>;
+
+export type PostSpell = BaseCommand<"post_spell", { spell: CreateSpell }, void>;
