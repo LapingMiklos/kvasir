08b84dc9879a1487dfc281bce9c06a52a9dab4f2
Laping Miklos-Jozsef <lapingmikus@gmail.com>
2024-06-13 22:39:58 +0300

    fixed window size issue

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,15 @@ import SideBar from "./components/SideBar";
 
 const App: Component<{ children?: JSX.Element }> = ({ children }) => {
   return (
-    <>
+    <div
+      style={{ height: "100%", display: "flex", "flex-direction": "column" }}
+    >
       <TitleBar />
-      <div style={{ display: "flex" }}>
+      <div style={{ display: "flex", flex: 1 }}>
         <SideBar />
         <div style={{ flex: 1 }}>{children}</div>
       </div>
-    </>
+    </div>
   );
 };
 
